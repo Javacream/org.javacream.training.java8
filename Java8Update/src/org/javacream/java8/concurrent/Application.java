ddb7f0bb8a6068c78e7425af590ad2e61fea1c2f
Rainer Sawitzki <training@rainer-sawitzki>
2019-01-22 15:07:29 +0100

    add concurrent example

--- Java8Update/src/org/javacream/java8/concurrent/Application.java
+++ Java8Update/src/org/javacream/java8/concurrent/Application.java
@@ -4,18 +4,16 @@ import java.util.concurrent.CompletableFuture;
 
 public class Application {
 
-	public static void main(String[] args) throws Exception {
-		System.out.println("result = " + productOfSumAndDiff1Simple(3));
-	}
+    public static void main(String[] args) throws Exception {
+	System.out.println("result = " + productOfSumAndDiff1Simple(3));
+    }
 
+    static int productOfSumAndDiff1Simple(int value) throws Exception {
+	final CompletableFuture<Integer> f1 = CompletableFuture.supplyAsync(() -> value);
+	final CompletableFuture<Integer> f2 = f1.thenApplyAsync(x -> x + 1);
+	final CompletableFuture<Integer> f3 = f1.thenApplyAsync(x -> x - 1);
+	final CompletableFuture<Integer> f4 = f2.thenCombine(f3, (v1, v2) -> v1 * v2);
+	return f4.get();
+    }
 
-	static int productOfSumAndDiff1Simple(int value) throws Exception {
-		final CompletableFuture<Integer> f1 = CompletableFuture.supplyAsync(() -> value);
-		final CompletableFuture<Integer> f2 = f1.thenApplyAsync(x -> x + 1);
-		final CompletableFuture<Integer> f3 = f1.thenApplyAsync(x -> x - 1);
-		final CompletableFuture<Integer> f4 = f2.thenCombine(f3, (v1, v2) -> v1 * v2);
-		return f4.get();
-	}
-
-	
 }
